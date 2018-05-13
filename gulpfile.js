0ccddea8da82fafb75147b43fc643ef876f2f932
mrKing <MrKing>
2018-05-13 19:26:57 +0300

    img working, slider not working

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const gulpWebpack = require('gulp-webpack');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
 
 
 
@@ -18,12 +21,27 @@ const paths = {
     style: {
         src: 'src/style/**/*.scss',
         dest: 'build/styles'
+    },
+
+    images: {
+        src: 'src/images/**/*',
+        dest: 'build/images'
+    },
+    scripts: {
+        src: 'src/scripts/**/*',
+        dest: 'build/scripts'
     }
 }
 
 //oчистка
 function clean() {
     return del(paths.root);
+}
+
+// Переносим картинки
+function images () {
+    return gulp.src(paths.images.src)
+        .pipe(gulp.dest(paths.images.dest));
 }
 
 //pug
@@ -47,7 +65,9 @@ function styles() {
 //слежка
 function watch() {
     gulp.watch(paths.style.src, styles);
-    gulp.watch(paths.templates.src, templates)
+    gulp.watch(paths.templates.src, templates);
+    gulp.watch(paths.images.src, images);
+    gulp.watch(paths.scripts.src, scripts);
 }
 
 //следим за build и релоадим браузер
@@ -58,13 +78,21 @@ function server() {
     browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
 }
 
+//Webpack
+function scripts(){
+    return gulp.src('./src/scripts/app.js')
+    .pipe(gulpWebpack(webpackConfig, webpack))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
 exports.templates = templates;
 exports.styles = styles;
 exports.clean = clean;
+exports.images = images;
 
 gulp.task('default', gulp.series(
     clean,
-    gulp.parallel(styles, templates),
+    gulp.parallel(styles, templates, images, scripts),
     gulp.parallel(watch, server)
 ));
 
